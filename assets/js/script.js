cd3f0a0535c56b25dd50bfcadea6fb242ec7e90c
taylorkeltgen <taylorkeltgen@gmail.com>
2021-10-29 21:01:00 -0500

    setup for reservation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
+var arrivalInput = $("input[name='arrival']").value;
+var departureInput = $("input[name='departure']").value;
+var guestCountInput = $("select[name='value']").value;
+var roomTypeInput = $("select[name='room-type']").value;
+var buttonEl = $("#submit-btn");
+
+// Date selector
 $( function() {
-    $( "#datepicker" ).datepicker();
+  var minDate = new Date();
+    $( ".datepicker" ).datepicker({
+      showAnim: 'bounce',
+      minDate: minDate
+    });
   } );
+
+// Guest Head Count
+$( function() {
+  var spinner = $( "#spinner" ).spinner();
+} );
+
+
+buttonEl.addEventListener('click', () => {
+    localStorage.setItem('Arrival Date', arrivalInput);
+    localStorage.setItem('Departure Date', departureInput);
+    console.log(arrivalInput);
+});
+  
