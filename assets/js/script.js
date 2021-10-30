5bd46ad57ba24840833f489491ee76826fa22d78
taylorkeltgen <taylorkeltgen@gmail.com>
2021-10-30 14:28:26 -0500

    datepicker data logs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,51 @@
-var arrivalInput = $("input[name='arrival']").value;
-var departureInput = $("input[name='departure']").value;
-var guestCountInput = $("select[name='value']").value;
-var roomTypeInput = $("select[name='room-type']").value;
-var buttonEl = $("#submit-btn");
+// var arrivalInput = document.querySelector("input[name='arrival']").textContent;
+var reservationObj = {
+  arrival: 'dd/mm/yyyy',
+  depart: 'dd/mm/yyyy',
+  guest: guestCountInput,
+  roomType: roomTypeInput
+};
+var buttonEl = document.querySelector("#submit-btn");
+var guestCountInput = document.querySelector("#spinner").value;
+var roomTypeInput = document.querySelector("#room").value;
+
 
 // Date selector
 $( function() {
   var minDate = new Date();
-    $( ".datepicker" ).datepicker({
+    $( '#arrivalPicker' ).datepicker({
       showAnim: 'bounce',
       minDate: minDate
     });
-  } );
+    $('#arrivalPicker').on("change",function(){
+      reservationObj.arrival = $(this).val();
+      console.log(reservationObj);
+    });
+
+    $( '#departPicker' ).datepicker({
+      showAnim: 'bounce',
+      minDate: minDate
+    });
+    $('#departPicker').on("change",function(){
+      reservationObj.depart = $(this).val();
+      console.log(reservationObj);
+    });
+    console.log(reservationObj);
+} );
 
 // Guest Head Count
 $( function() {
   var spinner = $( "#spinner" ).spinner();
+  reservationObj.guest = $(this).val();
 } );
 
-
+console.log(reservationObj);
 buttonEl.addEventListener('click', () => {
-    localStorage.setItem('Arrival Date', arrivalInput);
-    localStorage.setItem('Departure Date', departureInput);
-    console.log(arrivalInput);
+    // localStorage.setItem(JSON.stringify(reservationObj));
+    console.log(reservationObj);
+    $( '#arrivalPicker' ).datepicker() = '';
+    $( '#departPicker' ).datepicker() = '';
+    guestCountInput = '';
+    roomTypeInput = '';
 });
   
